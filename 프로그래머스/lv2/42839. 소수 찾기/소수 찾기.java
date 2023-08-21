9bd898934e959091b2c73052b3ef24e4aa9b7a06
youngh0 <cyh6099@gmail.com>
2023-08-21 20:51:31 +0900

    [level 2] Title: 소수 찾기, Time: 4.30 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,63 @@
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean[] visited = new boolean[9];
+    public Set<Integer> nums = new HashSet<>();
+    public int maxLength;
+    public int answer = 0;
+
+    public int solution(String numbers) {
+        maxLength = numbers.length();
+
+        for (int i = 1; i < maxLength + 1; i++) {
+            backTracking(0, new StringBuilder(), i, numbers);
+        }
+
+        for (Integer num : nums) {
+            if (isPrime(num)) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+
+    public void backTracking(int index, StringBuilder cur, int maxIndex, String totalNumbers) {
+        if (cur.length() == maxIndex) {
+            nums.add(Integer.parseInt(String.valueOf(cur)));
+            return;
+        }
+
+        for (int i = 0; i < maxLength; i++) {
+            if (!visited[i]) {
+                cur.append(totalNumbers.charAt(i));
+                visited[i] = true;
+                backTracking(index, cur, maxIndex, totalNumbers);
+                visited[i] = false;
+                cur.deleteCharAt(cur.length() - 1);
+            }
+        }
+    }
+
+    public boolean isPrime(Integer num) {
+        System.out.println(num);
+        if (num == 0 || num == 1) {
+            return false;
+        }
+
+        int limit = (int) Math.sqrt(num);
+
+        for (int i = 2; i <= limit; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
