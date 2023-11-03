dc927acddd1add89eaf7e427918881d31201ff3f
youngh0 <cyh6099@gmail.com>
2023-11-03 17:50:35 +0900

    [level 3] Title: 입국심사, Time: 0.38 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,64 +1,28 @@
-// import java.util.*;
-
-// class Solution {
-//     public long solution(int n, int[] times) {
-        
-//         long left = 1;
-//         Arrays.sort(times);
-//         long right = times[times.length-1] * (long)n;
-//         long answer = right;
-//         while(left <= right){
-//             long mid = (left + right) / 2;
-//             int count = 0;
-            
-//             for(int time: times){
-//                 count += mid / time;
-//             }
-            
-//             if(count < n){
-//                 left = mid + 1;
-//             }else{
-//                 // answer = Math.min(answer, mid);
-//                 right = mid - 1;
-//                 answer = mid;
-//             }
-//         }
-        
-//         return answer;
-//     }
-// }
 import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
+        
+        long left = 1;
         Arrays.sort(times);
-        long min = 1;
-
-        // times 배열의 최악의 경우는 범위
-        // n명 기준으로 times의 가장 마지막 시간까지 탐색하는 경우.
-    	long max = (long) times[times.length-1]*n;
-    	long mid = 0;
-    	long sum;
-    	long answer = max;
-
-    	while(min <= max) {
-    		sum = 0;
-    		mid = (min + max) / 2;
-
-    		for(int time : times) {
-    			sum += mid / time;
-    		}
-
-    		if(sum >= n) {
-				answer = mid;
-				max = mid - 1;
-    		}
-    		else {
-    			min = mid + 1;
-    		}
-    	}
-
-
+        long right = (long) times[times.length-1] * n;
+        long answer = right;
+        while(left <= right){
+            long mid = (left + right) / 2;
+            long count = 0;
+            
+            for(int time: times){
+                count += mid / time;
+            }
+            
+            if(count < n){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+                answer = mid;
+            }
+        }
+        
         return answer;
     }
 }
