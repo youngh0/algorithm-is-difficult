9bee904fc8c6f6cdf7ba08fe4c48b03ceb9ebce9
youngh0 <cyh6099@gmail.com>
2023-11-07 14:48:59 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 62.56 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 3] 연속 펄스 부분 수열의 합 - 161988 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988?language=java) 
 
 ### 성능 요약
 
-메모리: 127 MB, 시간: 44.48 ms
+메모리: 135 MB, 시간: 62.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 14:45:2
+2023년 11월 2일 14:48:57
 
 ### 문제 설명
 
--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -8,17 +8,15 @@ class Solution {
         
         dp[0][0] = Math.max(sequence[0], 0);
         dp[0][1] = Math.max(-1 * sequence[0], 0);
+        answer = Math.max(dp[0][0], dp[0][1]);
         
         for(int i = 1;i < sequence.length;i++){
             dp[i][0] = Math.max(dp[i-1][1] + sequence[i], 0);
             dp[i][1] = Math.max(dp[i-1][0] + (-1 * sequence[i]), 0);
-        }
-        
-        for(int i = 0;i < sequence.length;i++){
-            // System.out.print(ln)
             long maxNum = Math.max(dp[i][0], dp[i][1]);
-            answer = Math.max(maxNum, answer);
+            answer = Math.max(answer, maxNum);
         }
+    
         return answer;
     }
 }
