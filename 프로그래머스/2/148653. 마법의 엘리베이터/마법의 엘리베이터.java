e1142614adf83ae0b422c4894e77325855e34b93
youngh0 <cyh6099@gmail.com>
2023-12-04 17:33:47 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.47 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,6 +1,6 @@
 class Solution {
     int answer = Integer.MAX_VALUE;
-    boolean flag = false;
+
     public int solution(int storey) {
     
         dfs(storey, 0, 0);
@@ -12,18 +12,14 @@ class Solution {
             return;
         }
         if(s == 0){
-            // System.out.println("count: " + count + " s: " + s);
             answer = Math.min(answer, count);
             return;
         }
         String st = String.valueOf(s);
         
-        
-        
         int plus = s + (10 - (st.charAt(st.length() -1 - depth) - '0')) * (int)Math.pow(10, depth);
         int minus = s - ((st.charAt(st.length() -1 - depth) - '0')) * (int)Math.pow(10, depth);
-        // System.out.println(plus);
-        // System.out.println(minus);
+        
         dfs(plus, count + (10 - (st.charAt(st.length() -1 - depth) - '0')),depth+1);
         dfs(minus, count + ((st.charAt(st.length() -1 - depth) - '0')),depth+1);
     }
