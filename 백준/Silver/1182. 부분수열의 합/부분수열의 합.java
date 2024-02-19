3aa0a8addf2291fd7a4ac3db8bb42c2181d3a296
youngh0 <cyh6099@gmail.com>
2024-02-19 15:09:16 +0900

    [Silver II] Title: 부분수열의 합, Time: 140 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,43 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] nums;
+    static int answer = 0;
+
+    static int n;
+    static int s;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        backTracking(0, 0, 0);
+        System.out.println(answer);
+    }
+
+    public static void backTracking(int sum, int cnt, int used) {
+        if (cnt == n) {
+            if (sum == s && used > 0) {
+                answer++;
+            }
+            return;
+        }
+
+        backTracking(sum + nums[cnt], cnt + 1, used + 1);
+        backTracking(sum, cnt + 1, used);
+    }
+}
