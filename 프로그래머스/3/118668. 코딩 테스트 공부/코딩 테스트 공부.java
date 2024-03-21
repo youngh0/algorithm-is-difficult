4056ade8b8c77e31a5ca061e72a6f9cc8b1d2131
youngh0 <cyh6099@gmail.com>
2024-03-22 00:56:08 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 1.43 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,47 +1,47 @@
 import java.util.*;
 
 class Solution {
-    static final int INF = 1_000_000; // 대충 큰 값
-    int maxAlp; // 문제 중 가장 높은 알고력 수치
-    int maxCop; // 문제 중 가장 높은 코딩력 수치
-
     public int solution(int alp, int cop, int[][] problems) {
-        for (int[] problem : problems) {
-            maxAlp = Math.max(maxAlp, problem[0]); 
-            maxCop = Math.max(maxCop, problem[1]); 
+        int answer = 0;
+        // max alp, max cop 구하기
+        int maxAlp = 0;
+        int maxCop = 0;
+        
+        for(int[] problem: problems){
+            maxAlp = Math.max(problem[0], maxAlp);
+            maxCop = Math.max(problem[1], maxCop);
         }
-
-        // dp[alp][cop] = (alp,cop) 위치까지 도달하는데 걸리는 최단 시간
-        int[][] dp = new int[maxAlp + 1][maxCop + 1];
-
-        alp = Math.min(alp, maxAlp); // alp가 이미 maxAlp보다 더 클 수 있음 -> 오버플로우 발생 방지
-        cop = Math.min(cop, maxCop); // cop도 동일
-
-        for (int[] ints : dp) {
-            Arrays.fill(ints, INF);
+        
+        if(maxAlp <= alp && maxCop <= cop){
+            return 0;
         }
-
-        dp[alp][cop] = 0; // 시작 위치
-
-        for(int i = alp; i <= maxAlp; i++) {
-            for(int j = cop; j <= maxCop; j++) {
-                // 기본 문제 풀 경우
-                if(i + 1 <= maxAlp) {
-                    dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + 1);
-                }
-                if(j + 1 <= maxCop) {
-                    dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + 1);
-                }
-                // 선택 문제 풀 경우
-                for (int[] problem : problems) {
-                    if(problem[0] <= i && problem[1] <= j) { // 풀 수 있는 경우
-                        int nextAlp = Math.min(maxAlp, i + problem[2]); // maxAlp 범위를 초과하는 경우 
-                        int nextCop = Math.min(maxCop, j + problem[3]); // maxCop 범위를 초과하는 경우 
-                        dp[nextAlp][nextCop] = Math.min(dp[nextAlp][nextCop], dp[i][j] + problem[4]);
+        
+        int[][] dp = new int[maxAlp + 2][maxCop + 2];
+        for(int[] d : dp){
+            Arrays.fill(d, Integer.MAX_VALUE);
+        }
+        
+        
+        
+        alp = Math.min(alp, maxAlp);
+        cop = Math.min(cop, maxCop);
+        dp[alp][cop] = 0;
+        for(int i = alp; i < maxAlp + 1;i++){
+            for(int j = cop; j < maxCop + 1; j++){
+                dp[i+1][j] = Math.min(dp[i][j] + 1, dp[i+1][j]);
+                dp[i][j+1] = Math.min(dp[i][j] + 1, dp[i][j+1]);
+                
+                for(int[] problem: problems){
+                    if(i >= problem[0] && j >= problem[1]){
+                        
+                        int alpLevel = Math.min(i + problem[2], maxAlp);
+                        int copLevel = Math.min(j + problem[3], maxCop);
+                        dp[alpLevel][copLevel] = Math.min(dp[i][j] + problem[4], dp[alpLevel][copLevel]);
                     }
                 }
             }
         }
         return dp[maxAlp][maxCop];
+        
     }
 }
