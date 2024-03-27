de264cc0f8e4bac942e6ceb4a5721db7f9281105
youngh0 <cyh6099@gmail.com>
2024-03-27 16:55:22 +0900

    [level 3] Title: 등산코스 정하기, Time: 174.50 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,87 +1,97 @@
 import java.util.*;
 
 class Solution {
-    static List<ArrayList<Edge>> graph = new ArrayList<>();
-    static HashSet<Integer> gate = new HashSet<>();
-    static HashSet<Integer> summit = new HashSet<>();
-    static int[] dp;
-    static final int INF = 1_000_000_000;
-
-    static class Edge {
-        int a;
-        int cost;
-
-        Edge(int a, int cost) {
-            this.a = a;
-            this.cost = cost;
+    
+    public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+        Set<Integer> gateSet = new HashSet<>();
+        Set<Integer> summitSet = new HashSet<>();
+        
+        for(int gate: gates){
+            gateSet.add(gate);
         }
+        
+        for(int summit: summits){
+            summitSet.add(summit);
+        }
+        
+        List<List<int[]>> map = new ArrayList<>();
+        
+        for(int i = 0; i < n+1;i++){
+            map.add(new ArrayList<>());
+        }
+        
+        for(int[] path: paths){
+            int start = path[0];
+            int end = path[1];
+            int cost = path[2];
+            
+            if(gateSet.contains(start)){
+                map.get(start).add(new int[]{end,cost});
+            }
+            else if(gateSet.contains(end)){
+                map.get(end).add(new int[]{start, cost});
+            }
+            else if(summitSet.contains(start)){
+                map.get(end).add(new int[]{start, cost});
+            }
+            else if(summitSet.contains(end)){
+                map.get(start).add(new int[]{end, cost});
+            }else{
+                map.get(start).add(new int[]{end, cost});
+                map.get(end).add(new int[]{start,cost});
+            }
+        }
+        
+        int[] distance = new int[n+1];
+        
+        // for(List<int[]> root: map){
+        //     for(int[] r: root){
+        //         System.out.print(r[0] + ": " + r[1] + ", ");
+        //     }
+        //     System.out.println();
+        // }
+        dijkstra(distance, gateSet, map);
+        
+        int[] answer = new int[]{0, Integer.MAX_VALUE};
+        Arrays.sort(summits);
+        for(int s: summits){
+            if(distance[s] < answer[1]){
+                answer = new int[]{s, distance[s]};
+            }
+            
+        }
+        return answer;
     }
-
-    static void bfs(int c) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {c, 0});
-
-        while (!queue.isEmpty()) {
-            int[] p = queue.poll();
-
-            if (summit.contains(p[0])) {
+    
+    public void dijkstra(int[] distance, Set<Integer> gateSet, List<List<int[]>> map){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        for(int gate: gateSet){
+            distance[gate] = 0;
+            pq.add(new int[]{gate, 0});
+        }
+        
+        while(!pq.isEmpty()){
+            int[] polled = pq.poll();
+            int start = polled[0];
+            int cost = polled[1];
+    
+            if(distance[start] < cost){
                 continue;
             }
-
-            for (Edge edge : graph.get(p[0])) {
-                if (!gate.contains(edge.a)) {
-                    int nextValue = Math.max(p[1], edge.cost);
-
-                    if (nextValue < dp[edge.a]) {
-                        queue.add(new int[] {edge.a, nextValue});
-                        dp[edge.a] = nextValue;
-                    }
+            
+            for(int[] next: map.get(start)){
+                int nextNode = next[0];
+                int nextCost = next[1];
+            
+                int maxValue = Math.max(nextCost, distance[start]);
+                if(maxValue < distance[nextNode]){
+                    
+                    distance[nextNode] = maxValue;
+                    pq.add(new int[]{nextNode, maxValue});
                 }
             }
         }
-    }
-
-    public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        dp = new int[n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-            dp[i] = INF;
-        }
-
-        for (int i = 0; i < gates.length; i++) {
-            gate.add(gates[i]); // gate 추가
-        }
-
-        for (int i = 0; i < summits.length; i++) {
-            summit.add(summits[i]);
-        }
-
-        for (int i = 0; i < paths.length; i++) {
-            graph.get(paths[i][0]).add(new Edge(paths[i][1], paths[i][2]));
-            graph.get(paths[i][1]).add(new Edge(paths[i][0], paths[i][2]));
-        }
-
-        int[] res = new int[] {0, INF};
-
-        for (Integer start : gate) {
-            bfs(start);
-        }
-
-        for (Integer mount : summit) {
-            if (res[1] >= dp[mount]) {
-                if (res[1] == dp[mount]) {
-                    if (res[0] > mount) {
-                        res[0] = mount;
-                        res[1] = dp[mount];    
-                    }
-                } else {
-                    res[0] = mount;
-                    res[1] = dp[mount];
-                }
-            }
-        }
-
-        return res;
     }
 }
